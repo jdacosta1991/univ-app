7f1a0f59bc36a91783e215534c6420b0756b3640
jdacosta1991 <workdacojo@gmail.com>
2022-04-12 21:51:34 -0400

    add table for many-to-many associations student-courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
     validates :name, presence: true, length: {minimum: 5, maximum: 50} 
     validates :description, presence: true, length: {minimum: 10, maximum: 150}
     validates :short_name, presence: true, length: {minimum: 3, maximum: 10}
+
+    has_many :student_courses
+    has_many :students, through: :student_courses
 end
